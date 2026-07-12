851b6e5836886002047371e050a4bb9cebca8c62
Rokua27 <davidrr2710@gmail.com>
2026-07-12 10:41:38 -0500

    Set up HTTP server for TitansBot

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Titansbot iniciado correctamente");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('TitansBot esta funcionando');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`TitansBot iniciado en el puerto ${PORT}`);
+});
